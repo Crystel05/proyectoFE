b3379728040df8ef6ba8d548242ca6551084e2b4
Crystel Montero <crysvane05@estudiantec.cr>
2022-10-13 10:13:40 -0600

    changes

--- src/ReusableComponents/Fields/image_uploader.js
+++ src/ReusableComponents/Fields/image_uploader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FileUpload from "react-mui-fileuploader"
+//import FileUpload from "react-mui-fileuploader"
 
 export default function ImageUploader(){
     const handleFileUploadError = (error) => {
@@ -10,26 +10,26 @@ export default function ImageUploader(){
         // Do something...
       }
     
-      return (
-        <FileUpload
-          multiFile={false}
-          disabled={false}
-          title=""
-          header="Drag to drop"
-          leftLabel="or"
-          rightLabel="to select files"
-          buttonLabel="click here"
-          buttonRemoveLabel=""
-          maxFileSize={10}
-          maxUploadFiles={0}
-          maxFilesContainerHeight={357}
-          errorSizeMessage={'fill it or move it to use the default error message'}
-          allowedExtensions={['jpg', 'jpeg', 'png']}
-          onFilesChange={handleFilesChange}
-          onError={handleFileUploadError}
-          imageSrc={'path/to/custom/image'}
-          bannerProps={{ elevation: 0, variant: "outlined" }}
-          containerProps={{ elevation: 0, variant: "outlined" }}
-        />
-      )
+    //   return (
+    //     <FileUpload
+    //       multiFile={false}
+    //       disabled={false}
+    //       title=""
+    //       header="Drag to drop"
+    //       leftLabel="or"
+    //       rightLabel="to select files"
+    //       buttonLabel="click here"
+    //       buttonRemoveLabel=""
+    //       maxFileSize={10}
+    //       maxUploadFiles={0}
+    //       maxFilesContainerHeight={357}
+    //       errorSizeMessage={'fill it or move it to use the default error message'}
+    //       allowedExtensions={['jpg', 'jpeg', 'png']}
+    //       onFilesChange={handleFilesChange}
+    //       onError={handleFileUploadError}
+    //       imageSrc={'path/to/custom/image'}
+    //       bannerProps={{ elevation: 0, variant: "outlined" }}
+    //       containerProps={{ elevation: 0, variant: "outlined" }}
+    //     />
+    //   )
 }
